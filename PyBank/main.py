dafa21f44c769273ac627a53bc09130e04e57723
javrobs <javieroblesamar@gmail.com>
2023-01-21 03:00:07 +0800

    Comments added to main files

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,35 +3,34 @@ import os
 import csv
 
 #declare variables
-inputpath = os.path.join("Resources","budget_data.csv")
-outputpath = os.path.join("analysis","results.txt")
-proffitloss=[]  #variable storing values from csv file
-months=[]       #variable storing months from csv file
-changes=[]      #variable storing changes from month to month
+inputpath = os.path.join("Resources","budget_data.csv") #path to csv file for reading
+outputpath = os.path.join("analysis","results.txt")     #path to txt file for writing
+proffit=[]                                              #variable storing values from csv file
+months=[]                                               #variable storing months from csv file
+changes=[]                                              #variable storing changes from month to month
 
-with open(inputpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader)
-    for row in csvreader:
-        proffitloss.append(row[1])      #populate list with values
-        months.append(row[0])           #populate 
+with open(inputpath, newline="") as csvfile:    
+    csvreader = csv.reader(csvfile, delimiter=",")      #create file reader
+    next(csvreader)                                     #skip headers
+    for row in csvreader:                               
+        proffit.append(float(row[1]))                   #populate list with values while casting as floating point numbers
+        months.append(row[0])                           #populate list with months
 
-for i in range(len(proffitloss)):
-    proffitloss[i]=float(proffitloss[i])
+for i in range(len(proffit)):                           #skipping the first i, substract last proffit/loss from current one to see chain and store in changes list
     if i>0:
-        changes.append(proffitloss[i]-proffitloss[i-1])
+        changes.append(proffit[i]-proffit[i-1])
 
 
-output=[]
+output=[]                                               #initialize a value to cycle through string outputs
 
-output.append("Financial Analysis\n----------------------------")
-output.append(f"Total Months: {len(proffitloss)}")
-output.append(f"Total: ${sum(proffitloss):,.2f}")
-output.append(f"Average Change: {sum(changes)/len(changes):,.2f}")
-output.append(f"Greatest Increase in Profits: {months[changes.index(max(changes))+1]} (${max(changes):,.2f})")
-output.append(f"Greatest Decrease in Profits: {months[changes.index(min(changes))+1]} (${min(changes):,.2f})")
+output.append("Financial Analysis\n----------------------------")                                                   #text header
+output.append(f"Total Months: {len(proffit)}")                                                                      #length of proffit list to see amount of months
+output.append(f"Total: ${sum(proffit):,.2f}")                                                                       #the sum of all values in the proffit list, formatted with two decimal points.
+output.append(f"Average Change: {sum(changes)/len(changes):,.2f}")                                                  #divided the sum of all changes over the amount of changes from list, formatted with two decimal points
+output.append(f"Greatest Increase in Profits: {months[changes.index(max(changes))+1]} (${max(changes):,.2f})")      #look for the matching index +1 value in the months list corresponding to the maximum value in the changes list
+output.append(f"Greatest Decrease in Profits: {months[changes.index(min(changes))+1]} (${min(changes):,.2f})")      #look for the matching index +1 value in the months list corresponding to the minimum value in the changes list
 
-with open(outputpath, 'w', newline="") as textoutput:
+with open(outputpath, 'w') as textoutput:   #text should be stored in outputpath defined earlier, and overwrite any content in file results.txt every time the code is run. 
     for line in output:
-        textoutput.write(f"{line}\n")
-        print(line)
+        textoutput.write(f"{line}\n")                   #write a line of text and skip to the next line for every line in the output list
+        print(line)                                     #print out a line of text for each line in the output list
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,32 @@
+#import modules for csv and os path assignment
 import os
 import csv
 
-inputpath=os.path.join("Resources","election_data.csv")
-outputpath=os.path.join("analysis","results.txt")
-total_votes=0
-candidates=["Charles Casper Stockham","Diana DeGette","Raymon Anthony Doane"]
-votes=[0] * len(candidates)
+#declare variables
+inputpath=os.path.join("Resources","election_data.csv")                             #path to csv file for reading
+outputpath=os.path.join("analysis","results.txt")                                   #path to txt file for writing                                                                      
+candidates=[]                                                                       #initialize a list to store candidates found in csv
+votes=[]                                                                            #initialize a list to store votes for each candidate
 
 with open(inputpath,'r') as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
-    next(csvreader)
-    for row in csvreader:
-        total_votes += 1
-        for i,candidate in enumerate(candidates):
-            if row[2]==candidate:
-                votes[i]+=1
+    csvreader=csv.reader(csvfile,delimiter=",")                                     #create file reader
+    next(csvreader)                                                                 #skip headers
+    
+    for row in csvreader:                                                           #read through file row by row
+        if row[2] not in candidates:                                                #if candidate not in candidate list, then:
+            candidates.append(row[2])                                                   #add candidate
+            votes.append(0)                                                             #create a corresponding votes value
+        for i,candidate in enumerate(candidates):                                   #holding a single row, go through candidate list:
+            if row[2]==candidate:                                                       #if candidate in csv row is found in candidates list, then:
+                votes[i]+=1                                                                 #add 1 vote to the corresponding value in the vote list
 
-outputs=[]
-outputs.append(f"Election Results\n-------------------------")
-outputs.append(f"Total votes: {total_votes}\n-------------------------")
-for i,candidate in enumerate(candidates):
-    outputs.append(f"{candidate}: {votes[i]/total_votes*100:.3f}% ({votes[i]})")
-outputs.append(f"-------------------------\nWinner: {candidates[votes.index(max(votes))]}\n-------------------------")
+outputs=[]                                                                          #initialize a value to cycle through string outputs
+outputs.append(f"Election Results\n-------------------------")                      #text header
+outputs.append(f"Total votes: {sum(votes)}\n-------------------------")             #total votes is the sum of all values in the votes list
+for i,candidate in enumerate(candidates):                                           #for each candidate in the candidates list (3):
+    outputs.append(f"{candidate}: {votes[i]/sum(votes)*100:.3f}% ({votes[i]})")         #add one line that says the name, the percentage of votes i/total with centecimals and the votes in parentheses
+outputs.append(f"-------------------------\nWinner: {candidates[votes.index(max(votes))]}\n-------------------------")      #calculate winner by finding the index from highest value in votes, and retrieving the candidate with the same index
 
-with open(outputpath,'w',newline="") as textoutput:
-    [textoutput.write(f"{line}\n") for line in outputs]
-    [print(line) for line in outputs]
+with open(outputpath,'w',newline="") as textoutput:                                 #text should be stored in outputpath defined earlier, and overwrite any content in file results.txt every time the code is run. 
+    [textoutput.write(f"{line}\n") for line in outputs]                             #write a line of text and skip to the next line for every line in the output list
+    [print(line) for line in outputs]                                               #print out a line of text for each line in the output list
